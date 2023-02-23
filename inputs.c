c8d12c62f2db98f3732a8a57708023a5e3a12eec
Albin Sigge <albin@sigge.cc>
2023-02-23 11:35:19 +0100

    added button masks

--- inputs.c
+++ inputs.c
@@ -1,6 +1,11 @@
 #include <stdint.h>
 #include <pic32mx.h>
 
+#define BTN1_MASK 0x1
+#define BTN2_MASK 0x2
+#define BTN3_MASK 0x4
+#define BTN4_MASK 0x8
+
 int getsw(void){
     volatile int swValue = (PORTD >> 8) & 0xf;
     return swValue;
