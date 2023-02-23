59b96d25da3ec32172c04a0b25b320d89f056c2d
Gustav Eriksson <gustavke@kth.se>
2023-02-23 10:59:46 +0100

    New file with all logic related to the display

---
+++ display.c
@@ -0,0 +1,96 @@
+#include <pic32mx.h>
+#include <stdint.h>
+
+#define DISPLAY_VDD PORTFbits.RF6
+#define DISPLAY_VBATT PORTFbits.RF5
+#define DISPLAY_COMMAND_DATA PORTFbits.RF4
+#define DISPLAY_RESET PORTGbits.RG9
+
+
+#define DISPLAY_VDD_PORT PORTF
+#define DISPLAY_VDD_MASK 0x40
+#define DISPLAY_VBATT_PORT PORTF
+#define DISPLAY_VBATT_MASK 0x20
+#define DISPLAY_COMMAND_DATA_PORT PORTF
+#define DISPLAY_COMMAND_DATA_MASK 0x10
+#define DISPLAY_RESET_PORT PORTG
+#define DISPLAY_RESET_MASK 0x200
+
+#define DISPLAY_SIZE 128*4
+
+uint8_t displaybuffer[DISPLAY_SIZE] = {0};
+
+char textbuffer[4][16];
+
+void delay(int cyc) {
+	int i;
+	for(i = cyc; i > 0; i--);
+}
+
+uint8_t spi_send_recv(uint8_t data) {
+	while(!(SPI2STAT & 0x08));
+	SPI2BUF = data;
+	while(!(SPI2STAT & 0x01));
+	return SPI2BUF;
+}
+
+void display_init() {
+	DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
+	delay(10);
+	DISPLAY_VDD_PORT &= ~DISPLAY_VDD_MASK;
+	delay(1000000);
+	
+	spi_send_recv(0xAE);
+	DISPLAY_RESET_PORT &= ~DISPLAY_RESET_MASK;
+	delay(10);
+	DISPLAY_RESET_PORT |= DISPLAY_RESET_MASK;
+	delay(10);
+	
+	spi_send_recv(0x8D);
+	spi_send_recv(0x14);
+	
+	spi_send_recv(0xD9);
+	spi_send_recv(0xF1);
+	
+	DISPLAY_VBATT_PORT &= ~DISPLAY_VBATT_MASK;
+	delay(10000000);
+	
+	spi_send_recv(0xA1);
+	spi_send_recv(0xC8);
+	
+	spi_send_recv(0xDA);
+	spi_send_recv(0x20);
+	
+	spi_send_recv(0xAF);
+}
+
+void clearScreen(uint8_t * buffer)
+{	int i;
+	for (i = 0; i < 128*4; i++) {
+		buffer[i] = 0;}
+}
+
+void updateScreen(const uint8_t *data) {
+	int i, j;
+	
+	for(i = 0; i < 4; i++) {
+		DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(0x00); //set low nybble of column
+		spi_send_recv(0x10); //set high nybble of column
+
+		DISPLAY_COMMAND_DATA_PORT |= DISPLAY_COMMAND_DATA_MASK;
+		
+		for(j = 0; j < 128; j++)
+			spi_send_recv(data[i*128 + j]);
+	}
+
+    
+}
+
+void renderPoint(int x, int y){
+	int page = y/8;
+	displaybuffer[page * 128 + x] = (uint8_t)(1 << y - 8 * page);
+}
